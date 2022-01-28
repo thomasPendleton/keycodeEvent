a9997f3c2cbbb217c45ce2061d9cbe69e777c74c
Thomas <thomas6576@yahoo.com>
2022-01-28 15:59:04 -0800

    finished project

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const keyCode = document.querySelector('.key-code')
 const code = document.querySelector('.code')
 
 document.addEventListener('keypress', (e) => {
+  if(e.code === 'Space'){
+     key.style.height = '49px'; 
+  } else {
+      key.style.height = 'auto'
+  }
+    
   key.innerHTML = e.key.toUpperCase()
   keyCode.innerHTML = e.keyCode
   code.innerHTML = e.code
