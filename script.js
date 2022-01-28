d855496a77d4ac5f1dd8a46f558bb01b5f7a5fc2
Thomas <thomas6576@yahoo.com>
2022-01-28 15:07:12 -0800

    new project started

---
+++ script.js
@@ -0,0 +1,9 @@
+const key = document.querySelector('.key')
+const keyCode = document.querySelector('.key-code')
+const code = document.querySelector('.code')
+
+document.addEventListener('keypress', (e) => {
+  key.innerHTML = e.key.toUpperCase()
+  keyCode.innerHTML = e.keyCode
+  code.innerHTML = e.code
+})
